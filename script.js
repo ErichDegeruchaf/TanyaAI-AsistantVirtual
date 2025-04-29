ae5ea6ca325b129abcd14248a9e42e69b63e24d4
ErichDegeruchaf <erichdegeruchaff@gmail.com>
2025-04-29 07:07:32 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ const loadSavedChatHistory = () => {
         const userMessageHtml = `
 
             <div class="message__content">
-                <img class="message__avatar" src="assets/profile.png" alt="User avatar">
+                <img class="message__avatar" src="profile.png" alt="User avatar">
                <p class="message__text">${conversation.userMessage}</p>
             </div>
         
@@ -45,7 +45,7 @@ const loadSavedChatHistory = () => {
         const responseHtml = `
         
            <div class="message__content">
-                <img class="message__avatar" src="assets/gemini.svg" alt="Gemini avatar">
+                <img class="message__avatar" src="gemini.svg" alt="Gemini avatar">
                 <p class="message__text"></p>
                 <div class="message__loading-indicator hide">
                     <div class="message__loading-bar"></div>
@@ -182,7 +182,7 @@ const displayLoadingAnimation = () => {
     const loadingHtml = `
 
         <div class="message__content">
-            <img class="message__avatar" src="assets/gemini.svg" alt="Gemini avatar">
+            <img class="message__avatar" src="gemini.svg" alt="Gemini avatar">
             <p class="message__text"></p>
             <div class="message__loading-indicator">
                 <div class="message__loading-bar"></div>
@@ -219,7 +219,7 @@ const handleOutgoingMessage = () => {
     const outgoingMessageHtml = `
     
         <div class="message__content">
-            <img class="message__avatar" src="assets/profile.png" alt="User avatar">
+            <img class="message__avatar" src="profile.png" alt="User avatar">
             <p class="message__text"></p>
         </div>
 
